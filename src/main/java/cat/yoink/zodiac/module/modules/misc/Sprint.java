eb2fdb75265337b1d6747c231d141e839b617533
svintus-cool <68377942+svintus-cool@users.noreply.github.com>
2020-07-25 14:23:28 +0700

    wrote sprint

--- src/main/java/cat/yoink/zodiac/module/modules/misc/Sprint.java
+++ src/main/java/cat/yoink/zodiac/module/modules/misc/Sprint.java
@@ -1,12 +1,13 @@
 package me.cat.yoink.zodiac.module.modules.misc;
 
-import me.z.aurora.module.Module;
+import me.cat.yoink.zodiac.module.manager.module
 
 public class Sprint extends Module {
-    public Sprint() {
-        super("Sprint", Category.MOVEMENT, "Automatically sprint");
-    }
 
+ public Sprint() {
+
+  super("Sprint", "Automatically sprints.", Category.MISC, true);
+  }
     public void onUpdate(){
         if(mc.player.moveForward > 0 && !mc.player.isSprinting()){
             mc.player.setSprinting(true);
