c9aca0104b0a523a12206f42f2e06d1f677506e4
Katatje <57377704+Katatje@users.noreply.github.com>
2020-07-25 21:46:10 +0800

    Work on ClickGUI

--- src/main/java/cat/yoink/zodiac/gui/ClickGUI.java
+++ src/main/java/cat/yoink/zodiac/gui/ClickGUI.java
@@ -1,5 +1,7 @@
 package cat.yoink.zodiac.gui;
 
+import cat.yoink.zodiac.module.manager.module.Category;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 
 public class ClickGUI extends GuiScreen {
@@ -7,7 +9,7 @@ public class ClickGUI extends GuiScreen {
     int X = 100;
     int Y = 100;
     int W = 200;
-    int H = 300;
+    int H = 200;
 
     int xB = 0;
     int yB = 0;
@@ -16,6 +18,9 @@ public class ClickGUI extends GuiScreen {
 
     @Override
     public void drawScreen(int mX, int mY, float partialTicks) {
+
+        FontRenderer fR = mc.fontRenderer;
+
         if (d) {
             X = mX - xB;
             Y = mY - yB;
@@ -23,7 +28,19 @@ public class ClickGUI extends GuiScreen {
 
         drawRect(X, Y, X + W, Y + H, 0xff000000);
 
+        int cX = X + 10;
+        int cY = Y + 10;
+
+        for (Category c : Category.values()) {
+
+            drawString(fR, c.getName(), cX, cY, 0xffffffff);
+
+            cX += fR.getStringWidth(c.getName()) + 30;
+
+        }
+
     }
+
 
     @Override
     protected void mouseClicked(int mX, int mY, int mouseButton) {
