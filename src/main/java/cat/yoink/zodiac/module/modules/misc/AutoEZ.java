95b315ad50fe2174d0406eb96a0f2c6cc858d359
Katatje <57377704+Katatje@users.noreply.github.com>
2020-07-26 23:47:10 +0800

    Fix svintus's code

--- src/main/java/cat/yoink/zodiac/module/modules/misc/AutoEZ.java
+++ src/main/java/cat/yoink/zodiac/module/modules/misc/AutoEZ.java
@@ -1,10 +1,11 @@
-package cat.yoink.zodiac.module.modules.misc
+package cat.yoink.zodiac.module.modules.misc;
 
-import cat.yoink.zodiac.module.manager.Module
-import cat.yoink.zodiac.Client
-import cat.yoink.zodiac.event.handler.EventHandler
-import cat.yoink.zodiac.event.events.PacketEvent
-//commented due to brain issues import cat.yoink.zodiac.event.Listener
+import cat.yoink.zodiac.Client;
+import cat.yoink.zodiac.event.events.PacketEvent;
+import cat.yoink.zodiac.module.manager.module.Category;
+import cat.yoink.zodiac.module.manager.module.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,56 +13,64 @@ import net.minecraft.network.play.client.CPacketChatMessage;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+
+//commented due to brain issues import cat.yoink.zodiac.event.Listener
 
 /**
 
  * by svintus/floppa
 
  */
-public class AutoEZ extends module { 
-  
-  public AutoEZ() {
-    super ("AutoEZ", "sends a message in chat when you kill someone", Category.MISC, true);
-    
-  }
-      @EventHandler
-    private Listener<PacketEvent.Send> sendListener = new Listener<>(event -> {
+public class AutoEZ extends Module {
+
+    public AutoEZ() {
+        super("AutoEZ", "sends a message in chat when you kill someone", Category.MISC, true);
+    }
+
+    // TODO: Cannot resolve symbol 'targetedPlayers'
+
+
+    @EventHandler
+    private Listener<PacketEvent.Send> sendPacketListener = new Listener<>(event -> {
         if (mc.player != null) {
-            if (this.targetedPlayers == null) {
-                this.targetedPlayers = new ConcurrentHashMap();
+            if (targetedPlayers == null) {
+                targetedPlayers = new ConcurrentHashMap();
             }
 
             if (event.getPacket() instanceof CPacketUseEntity) {
-                CPacketUseEntity cPacketUseEntity = (CPacketUseEntity)event.getPacket();
+                CPacketUseEntity cPacketUseEntity = (CPacketUseEntity) event.getPacket();
+
                 if (cPacketUseEntity.getAction().equals(CPacketUseEntity.Action.ATTACK)) {
+
                     Entity targetEntity = cPacketUseEntity.getEntityFromWorld(mc.world);
+
                     if (targetEntity instanceof EntityPlayer) {
-                        this.addTargetedPlayer(targetEntity.getName());
+
+                        addTargetedPlayer(targetEntity.getName());
                     }
                 }
             }
         }
     });
+
     @EventHandler
     private Listener<LivingDeathEvent> livingDeathEventListener = new Listener<>(event -> {
         if (mc.player != null) {
-            if (this.targetedPlayers == null) {
-                this.targetedPlayers = new ConcurrentHashMap();
+            if (targetedPlayers == null) {
+                targetedPlayers = new ConcurrentHashMap();
             }
 
             EntityLivingBase entity = event.getEntityLiving();
             if (entity != null) {
                 if (entity instanceof EntityPlayer) {
-                    EntityPlayer player = (EntityPlayer)entity;
+                    EntityPlayer player = (EntityPlayer) entity;
                     if (player.getHealth() <= 0.0F) {
                         String name = player.getName();
-                        if (this.shouldAnnounce(name)) {
-                            this.doAnnounce(name);
+                        if (shouldAnnounce(name)) {
+                            doAnnounce(name);
                         }
 
                     }
@@ -71,59 +80,54 @@ public class AutoEZ extends module {
     });
 
     public void onEnable() {
-        this.targetedPlayers = new ConcurrentHashMap();
+        targetedPlayers = new ConcurrentHashMap();
         Client.EVENT_BUS.subscribe(this);
     }
 
     public void onDisable() {
-        this.targetedPlayers = null;
+        targetedPlayers = null;
         Client.EVENT_BUS.unsubscribe(this);
     }
 
     public void onUpdate() {
-            if (this.targetedPlayers == null) {
-                this.targetedPlayers = new ConcurrentHashMap();
-            }
+        if (targetedPlayers == null) {
+            targetedPlayers = new ConcurrentHashMap();
+        }
 
-            Iterator var1 = mc.world.getLoadedEntityList().iterator();
+        Iterator var1 = mc.world.getLoadedEntityList().iterator();
 
-            while(var1.hasNext()) {
-                Entity entity = (Entity)var1.next();
-                if (entity instanceof EntityPlayer) {
-                    EntityPlayer player = (EntityPlayer)entity;
-                    if (player.getHealth() <= 0.0F) {
-                        String name = player.getName();
-                        if (this.shouldAnnounce(name)) {
-                            this.doAnnounce(name);
-                            break;
-                        }
+        while (var1.hasNext()) {
+            Entity entity = (Entity) var1.next();
+            if (entity instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) entity;
+                if (player.getHealth() <= 0.0F) {
+                    String name = player.getName();
+                    if (shouldAnnounce(name)) {
+                        doAnnounce(name);
+                        break;
                     }
                 }
             }
+        }
 
-            targetedPlayers.forEach((namex, timeout) -> {
-                if ((int)timeout <= 0) {
-                    this.targetedPlayers.remove(namex);
-                } else {
-                    this.targetedPlayers.put(namex, (int)timeout - 1);
-                }
+        targetedPlayers.forEach((namex, timeout) -> {
+            if ((int) timeout <= 0) {
+                targetedPlayers.remove(namex);
+            } else {
+                targetedPlayers.put(namex, (int) timeout - 1);
+            }
 
-            });
+        });
     }
 
     private boolean shouldAnnounce(String name) {
-        return this.targetedPlayers.containsKey(name);
+        return targetedPlayers.containsKey(name);
     }
 
     private void doAnnounce(String name) {
         targetedPlayers.remove(name);
-        if(index >= (AutoEZmessages.size() - 1)) index = -1;
-            index++;
-        String message;
-        if(AutoGgMessages.size() > 0)
-            message = AutoEZMessages.get(index);
-        else
-            message = "you just got ezd by zodiac, gonn cry bout it?";
+
+        String message = "you just got ezd by zodiac, gonn cry bout it?";
 
         String messageSanitized = message.replaceAll("ยง", "").replace("{name}", name);
         if (messageSanitized.length() > 255) {
@@ -135,10 +139,11 @@ public class AutoEZ extends module {
 
     public void addTargetedPlayer(String name) {
         if (!Objects.equals(name, mc.player.getName())) {
-            if (this.targetedPlayers == null) {
-                this.targetedPlayers = new ConcurrentHashMap();
+            if (targetedPlayers == null) {
+                targetedPlayers = new ConcurrentHashMap();
             }
 
             targetedPlayers.put(name, 20);
         }
     }
+}
