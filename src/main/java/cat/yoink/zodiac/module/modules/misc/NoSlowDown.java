c6ccc672fa51ab899417c488a177ac5f6e9d51dc
flophooks incorporated <68377942+svintus-cool@users.noreply.github.com>
2020-07-29 15:05:03 +0700

    finished noslowdown

--- src/main/java/cat/yoink/zodiac/module/modules/misc/NoSlowDown.java
+++ src/main/java/cat/yoink/zodiac/module/modules/misc/NoSlowDown.java
@@ -2,4 +2,29 @@ package cat.yoink.zodiac.module.modules.misc
 
 import cat.yoink.zodiac.module.manager.Module
 import cat.yoink.zodiac.Client
-import me.zero//ok i continue later
+import cat.yoink.zodiac.event.handler.EventHandler
+import me.zero.alpine.listener.Listener
+
+public class NoSlowDown extends module {
+ public NoSlowDown() {
+   super("NoSlowDown", "doesnt slow when doin shit.", Category.MISC, true);
+
+    }
+    
+
+@EventHandler
+    private Listener<InputUpdateEvent> eventListener = new Listener<>(event -> {
+        if (mc.player.isHandActive() && !mc.player.isRiding()) {
+            event.getMovementInput().moveStrafe *= 5;
+            event.getMovementInput().moveForward *= 5;
+        }
+    });
+
+    public void onEnable(){
+        Client.EVENT_BUS.subscribe(this);
+    }
+
+    public void onDisable(){
+        Client.EVENT_BUS.unsubscribe(this);
+    }
+}
