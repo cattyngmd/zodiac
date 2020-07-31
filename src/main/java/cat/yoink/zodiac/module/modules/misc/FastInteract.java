3ca9dc0065ae4ac5cf2f7faebaa0c2dd2b28acee
flophooks incorporated <68377942+svintus-cool@users.noreply.github.com>
2020-07-31 18:26:36 +0700

    finished fastinteract

--- src/main/java/cat/yoink/zodiac/module/modules/misc/FastInteract.java
+++ src/main/java/cat/yoink/zodiac/module/modules/misc/FastInteract.java
@@ -1,2 +1,48 @@
+package cat.yoink.zodiac.module.modules.misc;
 
+import cat.yoink.zodiac.module.manager.Module;
+import cat.yoink.zodiac.module.manager.setting.Setting;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.init.Items;
+import net.minecraft.item.*;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.util.math.BlockPos;
 
+public class FastInteract extends module {
+  public FastInteract () {
+  super("FastInteract", "Interaction speed goes vroom", Category.MISC, true)
+  }
+  
+    Setting.b exp;
+    Setting.b crystals;
+    Setting.b blocks;
+    Setting.b everything;
+
+    public void setup(){
+        exp = this.registerB( "Exp", true);
+        crystals = this.registerB("Crystals", true);
+        blocks = this.registerB("Blocks", false);
+        everything = this.registerB("All", false);
+    }
+
+    public void onUpdate() {
+        if (FastUse.mc.player.getHeldItemMainhand().getItem() instanceof ItemExpBottle) {
+            if (this.exp.getValue()) {
+                FastUse.mc.rightClickDelayTimer = 0;
+            }
+        } else if (FastUse.mc.player.getHeldItemMainhand().getItem() instanceof ItemEndCrystal) {
+            if (this.crystals.getValue()) {
+                FastUse.mc.rightClickDelayTimer = 0;
+            }
+        } else if (Block.getBlockFromItem((Item) FastUse.mc.player.getHeldItemMainhand().getItem()).getDefaultState().isFullBlock()) {
+            if (this.blocks.getValue()) {
+                FastUse.mc.rightClickDelayTimer = 0;
+            }
+            } else if (this.everything.getValue() && !(FastUse.mc.player.getHeldItemMainhand().getItem() instanceof ItemBlock)) {
+                FastUse.mc.rightClickDelayTimer = 0;
+            }
+        }
+    }
