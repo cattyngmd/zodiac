f3d7c1b80fc36d589d4a7150bd34f3beab763e61
flophooks incorporated <68377942+svintus-cool@users.noreply.github.com>
2020-08-07 13:12:32 +0700

    when u start the game its shows ur hwid send me ur hwid and ill add it to pastebin

--- src/main/java/cat/yoink/zodiac/HWIDLOCK.java
+++ src/main/java/cat/yoink/zodiac/HWIDLOCK.java
@@ -41,7 +41,7 @@ public class HWID
 		try
 		{
 			StringBuilder sb = new StringBuilder( );
-			URL url = new URL( "сюда ссылку на пастебин" );
+			URL url = new URL( "https://pastebin.com/GkEPRNrf" );
 			HttpURLConnection connection = ( HttpURLConnection )url.openConnection( );
 			connection.setRequestProperty(
 					"User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11" );
