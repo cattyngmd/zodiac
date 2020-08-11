75d952d3538d7498a017e87d30c9c3a9e0a912bf
Katatje <57377704+Katatje@users.noreply.github.com>
2020-08-11 17:45:30 +0800

    HWID idk if it works

--- src/main/java/cat/yoink/zodiac/Client.java
+++ src/main/java/cat/yoink/zodiac/Client.java
@@ -8,8 +8,17 @@ import cat.yoink.zodiac.module.manager.setting.SettingManager;
 import cat.yoink.zodiac.util.RPC;
 import me.zero.alpine.EventBus;
 import me.zero.alpine.EventManager;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 /*
  * Written by yoink and svintus
@@ -39,7 +48,7 @@ public class Client
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init(FMLInitializationEvent event) throws NoSuchAlgorithmException, IOException
     {
         settingManager = new SettingManager();
         EventHandler = new EventHandler();
@@ -49,5 +58,18 @@ public class Client
         CommandManager.initialize();
         rpc = new RPC();
         SettingManager.getSettingByID(69);
+
+        String s = System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("COMPUTERNAME");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(s.getBytes());
+        byte[] hashedUuid = md.digest();
+        String a = DatatypeConverter.printHexBinary(hashedUuid).toLowerCase();
+        if (!new Scanner(new URL("https://pastebin.com/raw/GkEPRNrf").openStream(), "UTF-8").useDelimiter("\\A").next().contains(a))
+        {
+            e(); // should crash
+            Minecraft.getMinecraft().player.jump(); // Should crash too
+        }
     }
+
+    void e() {e();}
 }
